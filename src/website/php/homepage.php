3175fe281fd63badd38e0251145b6cb668f9d152
Evan Warburton <123onevan@live.com>
2019-02-04 16:19:46 -0800

    added basic file structure and started homepage

---
+++ src/website/php/homepage.php
@@ -0,0 +1,15 @@
+<?php session_start() ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title> ChatBot </title>
+	<link rel="stylesheet" href="../css/homepage.css">
+</head>
+<body>
+	<h1> ChatBot </h1>
+	<div class="main">
+		<p> Chatbot will go here </p>
+	</div>
+
+</body>
+</html>
